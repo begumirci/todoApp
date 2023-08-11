13a9c4c04e471a3f9d57ecf183b7f416b0518f48
Begüm <begumircci@outlook.com>
2023-08-12 01:58:39 +0300

    Count methodu ve clear butonu düzeltildi

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,32 +1,19 @@
 
 const todoForm = document.querySelector('.todo-form');
 const inputElement = document.querySelector('.todo-input');
-
 const todoList = document.querySelector('.todo-list');
 const todoItem = document.querySelector('.todo-item');
-
 const todos = document.querySelectorAll('span.todo-name');
-
-
-//const elements = document.querySelectorAll('.completed');
 const clearComplete = document.querySelector('.clearAll');
 const options = document.querySelectorAll('[name="option"]');
 const listCount = document.querySelector('#kalan');
-const footers = document.querySelector('.footer');
 
-
-
-let count = 0;
-listCount.innerText = count;
 
 
 todoForm.addEventListener('submit',(e) =>{
 
     e.preventDefault();
-    
-    if(inputElement.value === ''){
-        return;
-    }
+    if(inputElement.value === ''){return;}
     
     todoList.innerHTML += `<li class="todo-item">
                 <label>
@@ -36,65 +23,25 @@ todoForm.addEventListener('submit',(e) =>{
                 </label>
             </li>`;
        
-     
-    count +=1;
-    listCount.innerText = count;
     inputElement.value = '';
+    todoCounter();
     bindClicks();
     clearBtn();
-    //hideFooter();
-    
-    saveItem();
     edit();
-    
-    
+    saveItem();
 })
 
-todoList.addEventListener('dblclick',edit)
-function edit(e){
-
-    if(e.target.className === 'todo-item'){
-        let newTodo = document.cre
-    }
-    
-
-}
-
-// 1 Yeni eleman eklendiğinde eventleri bağlamak -- en mantıksızı bu 
-// delegre etmek
-//create element ile oluşturmak -- sağlıklı çalısması icin mutlaka data havuzuna baglanması lazım
-//event delegation
-
-/*
-todoList.addEventListener('click',deleteTodo);
-function deleteTodo(e){
-
-    const targetEl = e.target;
-    if(targetEl.classList.contains('del-btn')){
-        removeTodo(targetEl)
-        count -=1;
-        listCount.innerText = count;
-    }
-    if(targetEl.classList.toggle('toggle')){
-        markTodo(targetEl);
-    }
-    clearBtn();
-}*/
-
 for (const option of options) {
-    option.addEventListener('click',dondur);
+    option.addEventListener('click',select);
 }
 
-function dondur(){
+function select(){
     todoList.classList.value = 'todo-list ' + this.value;
 }
-
-//eğer en asagıdaki blink fonsksiyonunu kullanırsın burası el değil this olacak./Ayrıca remove fonksiyonu da.
 
 todoList.addEventListener('dblclick',(e) =>{
     if(e.target.tagName === 'SPAN')
     edit(e.target);
-    
 })
 
 function edit(e){
@@ -115,78 +62,63 @@ function edit(e){
         }
         saveItem();
     })
-    
 }
 
 function markTodo(){
     this.parentElement.parentElement.classList.toggle('completed');
-    if(this.parentElement.parentElement.classList.contains('completed')){
-        count-=1;
-        listCount.innerText = count;
-        
-    }else{
-        count+=1;
-        listCount.innerText = count;
-    }
-
-    clearBtn();
+    showBtn();
+    todoCounter();
     saveItem();
 }
 
 function removeTodo(){
     this.parentElement.parentElement.remove();
-    count-=1;
-    listCount.innerText = count;
+    todoCounter();
     saveItem();
 }
 
-
-//bind-- bağlamak
+//Bütün clickleri bağlama fonskiyonu
 function bindClicks() {
 
     for (const btn of document.querySelectorAll('.del-btn')) {
         btn.innerText = 'del';
         btn.addEventListener('click',removeTodo)
     }
-
     for (const btn of document.querySelectorAll('.toggle')) {
         btn.addEventListener('click',markTodo);
     }
-
 }
 
-
 clearComplete.addEventListener('click',clearCompleted);
-function clearCompleted(){
+function clearCompleted() {
 
     for (const el of document.querySelectorAll('li.completed')) {
         el.remove();
+        todoCounter();
     }
+     saveItem();
 }
 
-function clearBtn (){
+function todoCounter(){
+    const todoCount = document.querySelectorAll('.todo-item:not(.completed)').length;
+    if(todoCount === 0 || todoCount === 1) {
+        listCount.innerText = `${todoCount} item left`;
+    }else {
+        listCount.innerText = `${todoCount} items left`;
+    }
+    saveItem();
+}
+
+function showBtn (){
     if(document.querySelector('li.completed') === null){
         clearComplete.classList.add('unvisible');
     }
     else{
         clearComplete.classList.remove('unvisible');
     }
-
+    saveItem();
 }
 
-
-
-
-function hideFooter() {
-    if(count === 0){
-        footers.classList.add('unvisible');
-    }else{
-        footers.classList.remove('unvisible');
-    }
-}
-
-clearBtn();
-//hideFooter();
 function saveItem(){
     localStorage.setItem("data",todoList.innerHTML)
 }
@@ -194,7 +126,15 @@ function saveItem(){
 function dataLoad(){
     todoList.innerHTML = localStorage.getItem("data");
 }
+
 dataLoad();
+showBtn();
+todoCounter();
+
+
+
+
+
 
 
 
